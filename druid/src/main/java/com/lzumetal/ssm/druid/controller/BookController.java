5e3af5e5dc26572204bf1f13bedccea7f6b61e63
liaosi <liaosilzu2007@163.com>
2018-01-22 18:15:42 +0800

    # druid

---
+++ druid/src/main/java/com/lzumetal/ssm/druid/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.lzumetal.ssm.druid.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author: liaosi
+ * @date: 2018-01-22
+ */
+@RestController
+public class BookController {
+}
---
+++ druid/src/main/java/com/lzumetal/ssm/druid/service/BookService.java
@@ -0,0 +1,13 @@
+package com.lzumetal.ssm.druid.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>Description: </p>
+ *
+ * @author: liaosi
+ * @date: 2018-01-22
+ */
+@Service
+public class BookService {
+}
