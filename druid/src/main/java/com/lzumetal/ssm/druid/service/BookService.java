a6055e03117165632b31900d0951d8b1d81532c1
liaosi <liaosilzu2007@163.com>
2018-01-23 18:48:01 +0800

    # druid的sql监控

--- druid/src/main/java/com/lzumetal/ssm/druid/service/BookService.java
+++ druid/src/main/java/com/lzumetal/ssm/druid/service/BookService.java
@@ -1,6 +1,11 @@
 package com.lzumetal.ssm.druid.service;
 
+import com.lzumetal.ssm.druid.entity.Book;
+import com.lzumetal.ssm.druid.mapper.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>Description: </p>
@@ -10,4 +15,15 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BookService {
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    public List<Book> getAllBooks() {
+        return bookMapper.selectAll();
+    }
+
+    public Book getById(Integer id) {
+        return bookMapper.getById(id);
+    }
 }
