a51bae1597f9fff6da7e0ad103138dfc9049bea9
liaosilzu2007 <liaosilzu2007@163.com>
2018-08-13 18:52:28 +0800

    # log

---
+++ log/src/main/java/com/lzumetal/ssm/logtest/Main.java
@@ -0,0 +1,13 @@
+package com.lzumetal.ssm.logtest;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        SysLoggers.mq.error("");
+
+    }
+
+
+}
---
+++ log/src/main/java/com/lzumetal/ssm/logtest/SysLoggers.java
@@ -0,0 +1,12 @@
+package com.lzumetal.ssm.logtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SysLoggers {
+
+    public static final Logger mq = LoggerFactory.getLogger("mq");
+
+    public static final Logger order = LoggerFactory.getLogger("order");
+
+}
